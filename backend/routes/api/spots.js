d11a262746c2f3a3246888797eee6bd3b6798e11
tyler <infinitespace20@gmail.com>
2022-10-26 16:17:16 -0500

    fix group by in get details of a spot by id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -111,9 +111,6 @@ router.get('/current', requireAuth, async (req, res) => {
             },
             {
                 model: SpotImage,
-                where: {
-                    preview:true
-                },
                 attributes: []
             }
         ],
@@ -168,7 +165,7 @@ router.get('/:spotId', async (req, res) => {
                 ]
             ]
         },
-        group: ['Spot.id']
+        group: ['Spot.id', 'SpotImages.id']
     })
 
     if(!spot) {
