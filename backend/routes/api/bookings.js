64f07528be1e732761e6e7ef345392a79aa0eceb
tyler <infinitespace20@gmail.com>
2022-10-27 17:25:22 -0500

    get current users bookings

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -7,6 +7,49 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+// get booking for current user
+router.get('/current', requireAuth, async (req, res) => {
+    const userId = req.user.id
+    const bookings = await Booking.findAll({
+        where: {
+            userId
+        }
+    })
 
+    let bookingInfo = []
+
+    for(let booking of bookings) {
+        booking = booking.toJSON()
+        const spot = await Spot.findByPk(booking.spotId, {
+            include: [
+                {
+                    model: SpotImage,
+                    where: {
+                        preview: true
+                    },
+                    required: false,
+                    attributes: []
+                }
+            ],
+            attributes: {
+                include: [
+                    [
+                        Sequelize.col('SpotImages.url'),
+                        'previewImage'
+                    ]
+                ],
+                exclude: [
+                    'updatedAt', 'createdAt'
+                ]
+            },
+            group: [ 'Spot.id', 'SpotImages.url' ]
+        })
+
+        booking.Spot = spot
+        bookingInfo.push(booking)
+    }
+
+    res.json({Bookings: bookingInfo})
+})
 
 module.exports = router
